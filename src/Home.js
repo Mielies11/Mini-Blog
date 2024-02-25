ec861522a58471ab41c65bf9284de0a8dd91d44e
Emile Mervin Burger <25275917@sun.ac.za>
2024-02-25 20:07:03 +0200

    useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const HomePage = () => {
 
     const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem ipsum...', author: 'Emile', id: 1},
-        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'Emile2', id: 2},
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Emile', id: 3}
+        { title: 'My new website', body: 'lorem ipsum...', author: 'Emile', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Emile2', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Emile', id: 3 }
     ]);
+
+    const [name, setName] = useState('Emile');
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+    {/*fetch date, comm with auth services
+    fires on every render
+    don't change state in here -> infinite loop of renders
+    Dependancies: specify when to run the effect
+*/}
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]); //dependency array
+
+    
     // const [name, setName] = useState('Emile');
     // const [age, setAge] = useState(22);
     // const handleClick = () => {
@@ -24,27 +37,29 @@ const HomePage = () => {
     // const handleClickAgain = (name) => {
     //     console.log('hello ' + name);
     // }
-    return (  
+    return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <BlogList blogs = {blogs.filter((blog) => blog.author === 'Emile')} title="Emile's Blogs" handleDelete={handleDelete}/>
-        {/* {blogs.map((blog) => (
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Emile')} title="Emile's Blogs" handleDelete={handleDelete} />
+            <button onClick={() => setName('New Emile')}>Change Name</button>
+            <p>{name}</p>
+            {/* {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>  
                 <h2>{blog.title}</h2>
                 <p>Written by {blog.author}</p>
                 </div>
             ))} */}
         </div>
-            /* <h2>Homepage</h2>
-            <p>{name}</p>
-            <p>{age}</p>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={handleClick}>It's your birthday</button>
-            { <button onClick = {() => {
-                handleClickAgain('Emile')
-            }}>More Dynamic Click</button>} */
-        
+        /* <h2>Homepage</h2>
+        <p>{name}</p>
+        <p>{age}</p>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={handleClick}>It's your birthday</button>
+        { <button onClick = {() => {
+            handleClickAgain('Emile')
+        }}>More Dynamic Click</button>} */
+
     );
 }
- 
+
 export default HomePage;
